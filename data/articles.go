f6e8e2bea0f734742fb43bcf007c15c1979a7216
Eoin McMahon <eoin.mcmahon.dev@gmail.com>
2021-07-25 15:51:50 +0200

    Added post method as well as put

--- data/articles.go
+++ data/articles.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+    "fmt"
     "time"
     "io"
     "encoding/json"
@@ -18,18 +19,61 @@ type Article struct {
 
 type Articles []*Article
 
+// Decodes body of request into a json object
+func (art *Article) FromJSON(reader io.Reader) error {
+    dec := json.NewDecoder(reader)
+    return dec.Decode(art)
+}
+
+// Encodes stored article into a json object
 func (art*Articles) ToJSON(writer io.Writer) error {
     enc := json.NewEncoder(writer)
     return enc.Encode(art)
 }
 
+// Return list of stored articles
 func GetArticles() Articles {
     return articleList
 }
 
+// Add an article to our article list
+func AddArticle(art *Article) {
+    art.ID = getNextID()
+    articleList = append(articleList, art)
+}
+
+// Update article with specific id 
+func UpdateArticle(id int, art *Article) error {
+    _, pos, err := findArticleByID(id)
+    if err != nil {
+        return err
+    }
+
+    art.ID = id
+    articleList[pos] = art
+
+    return err
+}
+
+func getNextID() int {
+    lastArticle := articleList[len(articleList)-1]
+    return lastArticle.ID + 1
+}
+
+var ErrArticleNotFound = fmt.Errorf("Article Not Found")
+
+func findArticleByID(id int) (*Article, int, error) {
+    for i, art := range articleList {
+        if art.ID == id {
+            return art, i, nil
+        }
+    }
+        return nil, -1, ErrArticleNotFound
+}
+
 var articleList = Articles {
     &Article{
-        ID:             001,
+        ID:             1,
         Title:          "Serial killer on death row Rodney Alcala dies of natural causes",
         Abstract:       "A man sentenced to death in the US state of California for murdering a 12-year-old girl and four other women has died of natural causes, officials say.",
         Body:           "Rodney Alcala, 77, died at a hospital near California's Corcoran state prison in the early hours of Saturday.Alcala, who was known as the 'Dating Game Killer' after taking part in a US TV show, was convicted in 2010.",
@@ -38,7 +82,7 @@ var articleList = Articles {
         WrittenOn:      time.Now().UTC().String(),
     },
     &Article{
-        ID:             002,
+        ID:             2,
         Title:          "Tokyo Olympics: What is 3x3 basketball all about, and who stood out on day one?",
         Abstract:       "It's been a long time coming, but on Saturday we finally saw what Olympic 3x3 basketball is all about. ",
         Body:           "One of four new sports for this year's Games - along with skateboarding, surfing and climbing - 3x3 is played on a half court, with both teams shooting at the same basket.",
--- handlers/articles.go
+++ handlers/articles.go
@@ -3,7 +3,9 @@ package handlers
 import (
     "log"
     "net/http"
+    "regexp"
     "github.com/Eoin-McMahon/NewsArticleRegistry/data"
+    "strconv"
 )
 type Articles struct {
     log *log.Logger
@@ -14,13 +16,55 @@ func NewArticle(log *log.Logger) *Articles {
 }
 
 func (article *Articles) ServeHTTP(resp_writer http.ResponseWriter, req *http.Request) {
+    // Handle GET Request
     if req.Method == http.MethodGet {
         article.getArticles(resp_writer, req)
         return
     }
 
+    // Handle POST Request
     if req.Method == http.MethodPost {
         article.addArticle(resp_writer, req)
+        return
+    }
+
+    // Handle PUT Request
+    if req.Method == http.MethodPut {
+        // expect an ID in the URI
+        path := req.URL.Path
+
+        pattern := `/([0-9]+)`
+        regex := regexp.MustCompile(pattern)
+        group := regex.FindAllStringSubmatch(path, -1)
+
+        // should only have one group in the match
+        if len(group) != 1 {
+            article.log.Println("Invalid URI - more than one id")
+            http.Error(resp_writer, "Invalid URI", http.StatusBadRequest)
+            return
+        }
+
+        // should only have 2 capture groups, the whole thing and the ID
+        if len(group[0]) != 2 {
+            article.log.Println("Invalid URI - more than one capture group")
+            http.Error(resp_writer, "Invalid URI", http.StatusBadRequest)
+            return
+        }
+
+        // Cast to int
+        idString := group[0][1]
+        id, err := strconv.Atoi(idString)
+
+        if err != nil {
+            article.log.Println("Invalid URI - could not cast id to int")
+            http.Error(resp_writer, "Invalid URI", http.StatusBadRequest)
+            return
+        }
+
+        article.log.Printf("Successfully retrieved id: %d", id)
+
+        article.updateArticle(id, resp_writer, req)
+
         return
     }
 
@@ -42,5 +86,37 @@ func (article *Articles) getArticles(resp_writer http.ResponseWriter, req *http.
 func (article *Articles) addArticle(resp_writer http.ResponseWriter, req *http.Request) {
     article.log.Println("Received POST request for article")
 
+    art := &data.Article{}
 
+    err := art.FromJSON(req.Body)
+    if err != nil {
+        http.Error(resp_writer, "Unable to unmarshal JSON from request", http.StatusBadRequest)
+    }
+    article.log.Printf("Article to store: %#v", art)
+
+    data.AddArticle(art)
+}
+
+func (article *Articles) updateArticle(id int, resp_writer http.ResponseWriter, req *http.Request) {
+    article.log.Println("Received PUT request for article")
+
+    art := &data.Article{}
+
+    err := art.FromJSON(req.Body)
+    if err != nil {
+        http.Error(resp_writer, "Unable to unmarshal JSON from request", http.StatusBadRequest)
+    }
+
+    article.log.Printf("Article to store: %#v", art)
+
+    err = data.UpdateArticle(id, art)
+    if err == data.ErrArticleNotFound {
+        http.Error(resp_writer, "Article Not Found", http.StatusNotFound)
+        return
+    }
+
+    if err != nil {
+        http.Error(resp_writer, "Article Not Found", http.StatusInternalServerError)
+        return
+    }
 }
